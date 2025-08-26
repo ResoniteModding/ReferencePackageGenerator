c96c7ccaf5dd48df9625771a7f9d0c018ab3fa47
haz <37149950+hazre@users.noreply.github.com>
2025-08-26 15:19:23 +0200

    Remove ExtensionAttribute in stripping (#1)
    * Remove ExtensionAttribute in stripping
    
    * refactor removing types

--- ReferencePackageGenerator/CodeStripper.cs
+++ ReferencePackageGenerator/CodeStripper.cs
@@ -11,6 +11,11 @@ namespace ReferencePackageGenerator
     public class CodeStripper
     {
         private static readonly Type _compilerGeneratedType = typeof(CompilerGeneratedAttribute);
+        
+        private static readonly HashSet<string> TypesToRemove = new HashSet<string>
+        {
+            "System.Runtime.CompilerServices.ExtensionAttribute"
+        };
 
         public AssemblyResolver Resolver { get; } = new();
 
@@ -21,10 +26,30 @@ namespace ReferencePackageGenerator
 
             foreach (var module in assembly.Modules)
             {
+                var typeDefsToRemove = new List<TypeDefinition>();
+                
                 foreach (var type in module.GetTypes())
                 {
+                    if (TypesToRemove.Contains(type.FullName))
+                    {
+                        typeDefsToRemove.Add(type);
+                        Console.WriteLine($"Removing type {type.FullName} from {Path.GetFileName(source)}");
+                        continue;
+                    }
+                    
                     foreach (var method in type.Methods)
                     {
+                        if (method.HasCustomAttributes)
+                        {
+                            var attrsToRemove = method.CustomAttributes
+                                .Where(ca => TypesToRemove.Contains(ca.AttributeType.FullName))
+                                .ToList();
+                            foreach (var attr in attrsToRemove)
+                            {
+                                method.CustomAttributes.Remove(attr);
+                            }
+                        }
+                        
                         if (/*UseEmptyMethodBodies && */method.HasBody)
                         {
                             var emptyBody = new Mono.Cecil.Cil.MethodBody(method);
@@ -33,6 +58,44 @@ namespace ReferencePackageGenerator
                             method.Body = emptyBody;
                         }
                     }
+                    
+                    if (type.HasCustomAttributes)
+                    {
+                        var attrsToRemove = type.CustomAttributes
+                            .Where(ca => TypesToRemove.Contains(ca.AttributeType.FullName))
+                            .ToList();
+                        foreach (var attr in attrsToRemove)
+                        {
+                            type.CustomAttributes.Remove(attr);
+                        }
+                    }
+                }
+                
+                if (module.HasCustomAttributes)
+                {
+                    var attrsToRemove = module.CustomAttributes
+                        .Where(ca => TypesToRemove.Contains(ca.AttributeType.FullName))
+                        .ToList();
+                    foreach (var attr in attrsToRemove)
+                    {
+                        module.CustomAttributes.Remove(attr);
+                    }
+                }
+                
+                if (assembly.HasCustomAttributes)
+                {
+                    var attrsToRemove = assembly.CustomAttributes
+                        .Where(ca => TypesToRemove.Contains(ca.AttributeType.FullName))
+                        .ToList();
+                    foreach (var attr in attrsToRemove)
+                    {
+                        assembly.CustomAttributes.Remove(attr);
+                    }
+                }
+                
+                foreach (var typeToRemove in typeDefsToRemove)
+                {
+                    module.Types.Remove(typeToRemove);
                 }
             }
             
